003c48d9a1114131912287939c4c14fa33b41acb
madhu <madhumithacuk1998@gmail.com>
2022-08-07 19:50:14 +0100

    Changes

--- src/main/java/com/example/demo/GreetingControllerTests.java
+++ src/main/java/com/example/demo/GreetingControllerTests.java
@@ -1,4 +1,3 @@
-
 package com.example.demo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,25 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-public class GreetingControllerTests {
-
-	@Autowired
-	private MockMvc mockMvc;
+class DemoApplicationTests {
 
 	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
-	}
-
-	@Test
-	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+	void contextLoads() {
 	}
 
 }
